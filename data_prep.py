3e9fbcb668383395673bdb49e963ba45e8b3a510
Vlad Mashtalier <vmashtaler5@gmail.com>
2024-07-06 19:07:07 +0300

    data preparation module refactored

--- data_prep.py
+++ data_prep.py
@@ -1,37 +1,11 @@
 import os
-import shutil
 import argparse
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def create_img_dir(df:pd.DataFrame, original_dir:str, new_dir:str) -> None:
-    errors = 0
-    os.makedirs(new_dir, exist_ok=True)
-    
-    for file_name in df['ImageId']:
-        source_file = os.path.join(original_dir, file_name)
-        destination_file = os.path.join(new_dir, file_name)
-
-        # Check if the source file exists and then copy it
-        if os.path.exists(source_file):
-            shutil.copy(source_file, destination_file)
-        else:
-            errors += 1
-            print(f"File not found: {source_file}")
-
-    if errors == 0:
-        print("~"*30)
-        print("FILES COPIED SUCCESSFULLY")
-        print("~"*30)
-    else:
-        print("~"*30)
-        print(f"FILES COPIED WITH {errors} errors")
-        print("~"*30)
-
-
-def rle_to_mask(rle_string:str, shape:tuple) -> np.array:
+def rle_to_mask(rle_string:str, shape:tuple) -> np.ndarray:
     s = rle_string.split()
     starts, lengths = [np.asarray(x, dtype=int) for x in (s[0::2], s[1::2])]
     starts -= 1
@@ -61,7 +35,7 @@ def mask_to_rle(mask:np.array) -> list:
     return rle
 
 
-def _combine_masks(rle_list:list, shape:tuple) -> np.array:
+def _combine_masks(rle_list:list, shape:tuple) -> np.ndarray:
     # Combine multiple RLEs into one mask
     combined_mask = np.zeros(shape, dtype=np.uint8)
     for rle in rle_list:
@@ -71,32 +45,16 @@ def _combine_masks(rle_list:list, shape:tuple) -> np.array:
     return combined_mask
 
 
-def create_mask(df:pd.DataFrame, file_name:str) -> np.array:
+def create_mask(df:pd.DataFrame, file_name:str, shape:tuple) -> np.ndarray:
 
     df = df.set_index("ImageId")
-    rle = df.loc[file_name, "Label"]
+    rle = df.loc[file_name, "EncodedPixels"]
     if isinstance(rle, pd.Series): rle = rle.tolist()
     else: rle = [rle]
-   
-    mask = _combine_masks(rle, (224, 224)).astype(np.float32)
+    print(len(rle))
+    mask = _combine_masks(rle, shape).astype(np.float32)
     return mask
 
-
-def create_dataset(df_data:pd.DataFrame) -> pd.DataFrame:
-    indxs = df_data["EncodedPixels"].isna()
-
-    for i, title in enumerate(df_data[~indxs]["ImageId"].unique()):
-        ids = df_data["ImageId"] == title
-        items = df_data[ids]
-        if len(items) > 1:
-            print(i)
-            encoding = items["EncodedPixels"].str.cat(sep=' ')
-            item = {"ImageId":title, "EncodedPixels":encoding}
-            df_data = df_data[~ids]
-            df_data = df_data.append(item, ignore_index=True)
-        
-        return df_data
-        
         
 def split_data(new_data_path:str, rle_path:str, blank_percentage:float) -> None:
     os.makedirs(new_data_path, exist_ok=True)
