c2d832dd4f068b611ba645e9dd09484561590909
Vlad Mashtalier <vmashtaler5@gmail.com>
2024-07-06 19:11:41 +0300

    preprocessing module updated

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,4 @@
+import cv2
 import albumentations as A
 
 def get_training_augmentation():
@@ -19,5 +20,7 @@ def get_validation_augmentation():
 
 
 def get_preprocessing(preprocessing_fn):
-    transforms = [A.Resize(height=224, width=224, always_apply=True, p=1), A.Lambda(image=preprocessing_fn)]
+    transforms = [A.Resize(height=224, width=224, always_apply=True, p=1, interpolation=cv2.INTER_CUBIC), 
+                  A.Lambda(image=preprocessing_fn, mask=preprocessing_fn)]
+
     return A.Compose(transforms)
